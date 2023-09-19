eda5a7e8a30b714fc98c94ad6772dd196bcc98fc
Renata Borges <renata.borges@db1.com.br>
2023-09-19 11:58:20 -0300

    CRUD Scaffolding

--- SalesWeb/Startup.cs
+++ SalesWeb/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using SalesWeb.Data;
 
 namespace SalesWeb
 {
@@ -24,6 +26,9 @@ namespace SalesWeb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<SalesWebContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
