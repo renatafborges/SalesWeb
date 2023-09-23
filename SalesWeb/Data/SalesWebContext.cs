28927d9830e6452eed41d3446f65c1fb6d2cccf0
Renata Borges <renata.borges@db1.com.br>
2023-09-23 15:52:51 -0300

    Other entities and second migration

--- SalesWeb/Data/SalesWebContext.cs
+++ SalesWeb/Data/SalesWebContext.cs
@@ -1,14 +1,19 @@
 using Microsoft.EntityFrameworkCore;
+using SalesWeb.Models;
 
 namespace SalesWeb.Data
 {
     public class SalesWebContext : DbContext
     {
-        public SalesWebContext (DbContextOptions<SalesWebContext> options)
-            : base(options)
+        public SalesWebContext
+        (
+            DbContextOptions<SalesWebContext> options
+        ) : base(options)
         {
         }
 
-        public DbSet<SalesWeb.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
