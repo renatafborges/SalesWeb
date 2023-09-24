48dd2f8ff93a1167adc284045859ba19ff10fd71
Renata Borges <renata.borges@db1.com.br>
2023-09-24 16:08:03 -0300

    Seller Details and eager loading

--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using SalesWeb.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWeb.Data;
 using SalesWeb.Models;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,7 +22,7 @@ namespace SalesWeb.Services
         
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
         
         public void Remove(int id)
