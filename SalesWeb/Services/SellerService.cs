dd2720d1631d9e599d0cf8b80357762b7bd9da38
Renata Borges <renata.borges@db1.com.br>
2023-09-24 15:43:06 -0300

    Delete Seller

--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -19,6 +19,18 @@ namespace SalesWeb.Services
             return _context.Seller.ToList();
         }
         
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+        
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
+        
         public void Insert(Seller obj)
         {
             _context.Add(obj);
