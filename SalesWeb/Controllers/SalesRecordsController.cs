ef92d78356e1beefc3a443d6896cec4832eb0974
Renata Borges <renata.borges@db1.com.br>
2023-09-27 20:01:38 -0300

    Preparing sales search navigation views

---
+++ SalesWeb/Controllers/SalesRecordsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWeb.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        // GET
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+        
+        // GET
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
